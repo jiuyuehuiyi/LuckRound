3c2c4de11e0c108690a16fc529f92724201c5bd9
踏歌长行 <1031484446@qq.com>
2018-06-30 11:29:28 +0800

    Initial Commit

---
+++ LuckRound/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuckRound
+//
+//  Created by 邓伟浩 on 2018/6/30.
+//  Copyright © 2018年 邓伟浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LuckRound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LuckRound
+//
+//  Created by 邓伟浩 on 2018/6/30.
+//  Copyright © 2018年 邓伟浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
